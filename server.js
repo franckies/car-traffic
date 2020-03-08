09270dd0e7fe2e4c619ddc3a22193b09f11e0cbb
franckies <fk.semeraro@gmail.com>
2020-03-08 11:12:43 +0100

    changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 //set port
-var port = process.env.PORT ||| 8080
+var port = process.env.PORT || 8080
 
 app.use(express.static(_dirname+"/public"));
 /routes
